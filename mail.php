0862ef0a7477a120532c8a3e70f7f80f1fe8b468
Mateusz P <mateuszp1988git@wp.pl>
2023-02-11 21:55:03 +0100

    one commit

---
+++ mail.php
@@ -0,0 +1,40 @@
+<?php
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+$mailheader = "From:".$name."<".$email.">\r\n";
+
+$recipient = "mateuszp1988@wp.pl";
+
+mail($recipient, $subject, $message, $mailheader) or die("Error!");
+
+echo'
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact form</title>
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@600&family=Poppins&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Dziękuję za skontaktowanie się ze mną. Wrócę do Ciebie tak szybko, jak to możliwe!</h1>
+        <p class="back"> <font size="6">Wróć do strony <a href="index.html">głównej</font></a>.</p>
+        
+    </div>
+</body>
+</html>
+
+
+
+';
+
+
+?>
